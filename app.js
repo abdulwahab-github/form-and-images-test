45c8fa5aefe923c5b29020655d1032aefd5d0326
abdulwahab-github <112512244+abdulwahab-github@users.noreply.github.com>
2022-09-29 22:41:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+// function validation(){
+//     var user = document.getElementById('user').value;
+//     var lastname = document.getElementById('lastuser').value;
+//     var contact = document.getElementById('contact').value;
+//     var password = document.getElementById('password').value;
+    
+//     var zipcode = document.getElementById('zipcode').value;
+   
+// if (user == "") {
+//     document.getElementById('username').innerHTML = "please fill the username field";
+//        return false;
+    
+// }
+// if (lastname == "") {
+//    document.getElementById('lastname').innerHTML = "please fill the Lastname field";
+//    return false;
+// }
+
+// if (contact == "") {
+//     document.getElementById('number').innerHTML = "please fill the contact field";
+//     return false;
+//  }
+//  if (isNaN(contact)) {
+//     document.getElementById('number').innerHTML = "Only numbers are allowed ";
+//     return false;
+    
+//  }
+ 
+   
+
+
+// if (email == "") {
+//     document.getElementById('email5').innerHTML = "please fill the email field";
+//     return false;
+
+// }
+
+
+
+// if ( password == "") {
+//     document.getElementById('pass').innerHTML = "please fill the pass field";
+//     return false;
+//  }
+//  if (zipcode == "") {
+//     document.getElementById('zipcode1').innerHTML = "please fill the zipcode field";
+//     return false;
+
+
+// } 
+// }
+
+// ASSIGNMENT 2 
+
+
+
+// function abc(captain){
+//     var a = document.getElementById('tri')
+//     tri.src="image/indexz.png"
+// }
+// function ab(captain2){
+//     var a = document.getElementById("tri")
+//     tri.src = "image/index33.png"
+// }
+// function a(captain2){
+//     var a = document.getElementById("tri")
+//     tri.src = "image/index.png"
+// }
